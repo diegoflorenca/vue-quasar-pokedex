e93b8c20fa83edcebc0532b1f89da09c25938e2d
diegoflorenca <diegoflorenca@gmail.com>
2023-03-12 15:05:10 -0700

    feature: create pages and components

---
+++ src/components/PokemonDetails.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script setup>
+const msg = "This is the selected pokemon details!";
+</script>
---
+++ src/pages/PokedexPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="row q-gutter-md">
+      <div class="col"><PokemonDetails /></div>
+      <div class="col"><PokemonTable /></div>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import PokemonDetails from "src/components/PokemonDetails.vue";
+import PokemonTable from "src/components/PokemonTable.vue";
+</script>
