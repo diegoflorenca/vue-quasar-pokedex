9d73860ce26e0aac4cef2900f2137228d5a07524
diegoflorenca <diegoflorenca@gmail.com>
2023-03-12 20:38:45 -0700

    feature: watch selected pokemon from pokemonDetails component

--- src/components/PokemonDetails.vue
+++ src/components/PokemonDetails.vue
@@ -1,7 +1,25 @@
 <template>
-  <div>{{ msg }}</div>
+  <q-page class="row q-gutter-md">
+    <div>
+      <h3>Details</h3>
+      <p>
+        {{ pokemon }}
+      </p>
+    </div>
+  </q-page>
 </template>
 
 <script setup>
-const msg = "This is the selected pokemon details!";
+import { watch, reactive } from "vue";
+import { storeToRefs } from "pinia";
+import { usePokedexStore } from "src/stores/pokedex";
+const pokemonStore = usePokedexStore();
+const { selected } = storeToRefs(pokemonStore);
+
+const pokemon = reactive({});
+
+watch(selected, (currentSeletedPokemon) => {
+  pokemon.id = currentSeletedPokemon.id;
+  pokemon.name = currentSeletedPokemon.name;
+});
 </script>
--- src/stores/pokedex.js
+++ src/stores/pokedex.js
@@ -3,16 +3,14 @@ import { defineStore } from "pinia";
 export const usePokedexStore = defineStore("pokemon", {
   state: () => ({
     list: [],
-    selected: {
-      id: null,
-      name: "",
-    },
+    selected: {},
   }),
   getters: {
     getPokemonList: (state) => state.list,
     getPokemonById: (state) => {
       return (id) => state.list.find((pokemon) => pokemon.id === id);
     },
+    getSelectedPokemon: (state) => state.selected,
   },
   actions: {
     setPokemonList(list) {
