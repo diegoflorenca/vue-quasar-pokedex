47c92f2b5433b3df6e7fd9870a9fd51e3e10795d
diegoflorenca <diegoflorenca@gmail.com>
2023-03-25 08:39:51 -0700

    fix: fetching data using https

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,7 +7,7 @@ import axios from "axios";
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: "http://pokeapi.co/api/v2/" });
+const api = axios.create({ baseURL: "https://pokeapi.co/api/v2/" });
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
